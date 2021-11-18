6c6dbbac2cfb19022bd03e58ec1bf847a3c68878
t_den <t_den@inbox.ru>
2021-11-18 18:26:53 +0300

    Added User class

---
+++ src/main/java/com/github/tden27/bookingService/model/User.java
@@ -0,0 +1,9 @@
+package com.github.tden27.bookingService.model;
+
+public class User {
+    private String userName;
+    private String login;
+    private String password;
+
+    public User() {}
+}
